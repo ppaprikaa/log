1257724b22612972f1eccb0da30e754d432b8c1f
ppaprikaa <sanzhar.maratov@proton.me>
2024-05-07 17:36:47 +0500

    logger structure

---
+++ log.c
@@ -0,0 +1,42 @@
+#include "log.h"
+
+#define MAX_CALLBACKS 32
+
+#ifndef __STDC_NO_ATOMICS__
+#include "stdatomic.h"
+#endif
+
+typedef struct {
+	log_callback func;
+	log_level lvl;
+	FILE* writer;
+} callback;
+
+static struct {
+	// locking
+#ifdef __STDC_NO_ATOMICS__
+	int lock;
+#else
+	atomic_int lock;
+#endif
+
+	callback cbs[MAX_CALLBACKS];
+} logger;
+
+// in the absence of atomics, I want to provide at least some guarantees -.-
+static void lock() {
+#ifdef __STDC_NO_ATOMICS__
+	while (logger.lock);
+	logger.lock = 1;
+#else
+	while(atomic_exchange(&logger.lock, 1));
+#endif
+}
+
+static void unlock() {
+#ifdef __STDC_NO_ATOMICS__
+	logger.lock = 0;
+#else
+	atomic_store(&logger.lock, 0);
+#endif
+}
