ba96d30757509121ab821df87a55f76c1ef902e9
ppaprikaa <sanzhar.maratov@proton.me>
2024-05-07 16:15:08 +0500

    structure of log data

--- log.h
+++ log.h
@@ -3,8 +3,26 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <stdio.h>
+#include <time.h>
+
 enum LogLevel {
 	TRACE, DEBUG, INFO, WARN, ERROR, FATAL
 };
+
+typedef struct {
+	// writing destination
+	FILE* writer;
+	// log information
+	int lvl;
+	struct tm *time;
+	// caller information
+	char* caller_file;
+	int caller_line;
+	// message and arguments
+	char* fmt;
+	va_list args;
+} log;
+
 
 #endif
